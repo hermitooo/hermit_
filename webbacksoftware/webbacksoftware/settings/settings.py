cb77a74907b223e956a00bb8a58fa186c539f0c8
Muzhenyu <lvbu89757@163.com>
2019-06-17 05:33:58 -0700

    create one view

--- webbacksoftware/webbacksoftware/settings/settings.py
+++ webbacksoftware/webbacksoftware/settings/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user.apps.UserConfig'
+    'user.apps.UserConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
